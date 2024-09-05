df155f6fe64be30e62c69990379553c99dabc613
moomie1 <rileyk2809@gmail.com>
2024-09-04 17:58:24 -0700

    most templating works?

--- crm/views.py
+++ crm/views.py
@@ -18,7 +18,7 @@ class CreateUserView(CreateView):
 
 class DashboardView(ListView):
     model = CustomerInformation
-    template_name = 'dashboard.html'
+    template_name = 'General/dashboard.html'
     context_object_name = 'customers'
 
     def get_context_data(self, **kwargs):
@@ -33,23 +33,26 @@ class DashboardView(ListView):
         context['sales_funnel'] = CustomerLead.objects.values('lead_stage').annotate(
             count=Sum('id')
         )
+
         internal_services = InternalServices.objects.first()
-        context['growth_rate'] = internal_services.current_growth_rate
-        context['projected_revenue'] = internal_services.projected_revenue
-        context['churn_rate'] = internal_services.internal_churn_rate
+        context['growth_rate'] = internal_services.current_growth_rate if internal_services and internal_services.current_growth_rate is not None else "N/A"
+        context['projected_revenue'] = internal_services.projected_revenue if internal_services and internal_services.projected_revenue is not None else 0
+        context['churn_rate'] = internal_services.internal_churn_rate if internal_services and internal_services.internal_churn_rate is not None else "N/A"
+        
         return context
+
 
 
 class CustomerCreateView(CreateView):
     model = CustomerInformation
     form_class = CustomerForm
-    template_name = 'customer_add.html'
+    template_name = 'Customer/customer_add.html'
     success_url = reverse_lazy('dashboard')
 
 
 class CustomerDetailView(DetailView):
     model = CustomerInformation
-    template_name = 'customer_detail.html'
+    template_name = 'Customer/customer_detail.html'
     context_object_name = 'customer'
 
     def get_context_data(self, **kwargs):
@@ -70,26 +73,26 @@ class CustomerDetailView(DetailView):
 class CustomerUpdateView(UpdateView):
     model = CustomerInformation
     form_class = CustomerForm
-    template_name = 'customer_edit.html'
+    template_name = 'Customer/customer_edit.html'
     success_url = reverse_lazy('dashboard')
 
 
 class CustomerDeleteView(DeleteView):
     model = CustomerInformation
-    template_name = 'customer_confirm_delete.html'
+    template_name = 'Customer/customer_confirm_delete.html'
     success_url = reverse_lazy('dashboard')
 
 
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
-    template_name = 'product_add.html'
+    template_name = 'Product/product_add.html'
     success_url = reverse_lazy('dashboard')
 
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'product_detail.html'
+    template_name = 'Product/product_detail.html'
     context_object_name = 'product'
 
     def get_context_data(self, **kwargs):
@@ -104,26 +107,26 @@ class ProductDetailView(DetailView):
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
-    template_name = 'product_edit.html'
+    template_name = 'Product/product_edit.html'
     success_url = reverse_lazy('dashboard')
 
 
 class ProductDeleteView(DeleteView):
     model = Product
-    template_name = 'product_confirm_delete.html'
+    template_name = 'Product/product_confirm_delete.html'
     success_url = reverse_lazy('dashboard')
 
 
 class LeadCreateView(CreateView):
     model = CustomerLead
     form_class = LeadForm
-    template_name = 'lead_add.html'
+    template_name = 'Lead/lead_add.html'
     success_url = reverse_lazy('dashboard')
 
 
 class LeadDetailView(DetailView):
     model = CustomerLead
-    template_name = 'lead_detail.html'
+    template_name = 'Lead/lead_detail.html'
     context_object_name = 'lead'
 
     def get_context_data(self, **kwargs):
@@ -136,57 +139,57 @@ class LeadDetailView(DetailView):
 class LeadUpdateView(UpdateView):
     model = CustomerLead
     form_class = LeadForm
-    template_name = 'lead_edit.html'
+    template_name = 'Lead/lead_edit.html'
     success_url = reverse_lazy('dashboard')
 
 
 class LeadDeleteView(DeleteView):
     model = CustomerLead
-    template_name = 'lead_confirm_delete.html'
+    template_name = 'Lead/lead_confirm_delete.html'
     success_url = reverse_lazy('dashboard')
 
 
 class EngagementCreateView(CreateView):
     model = Engagement
     form_class = EngagementForm
-    template_name = 'engagement_add.html'
+    template_name = 'Engagement/engagement_add.html'
     success_url = reverse_lazy('dashboard')
 
 
 class EngagementDetailView(DetailView):
     model = Engagement
-    template_name = 'engagement_detail.html'
+    template_name = 'Engagement/engagement_detail.html'
     context_object_name = 'engagement'
 
 
 class EngagementUpdateView(UpdateView):
     model = Engagement
     form_class = EngagementForm
-    template_name = 'engagement_edit.html'
+    template_name = 'Engagement/engagement_edit.html'
     success_url = reverse_lazy('dashboard')
 
 
 class EngagementDeleteView(DeleteView):
     model = Engagement
-    template_name = 'engagement_confirm_delete.html'
+    template_name = 'Engagement/engagement_confirm_delete.html'
     success_url = reverse_lazy('dashboard')
 
 
 class ProductsPurchasedCreateView(CreateView):
     model = ProductsPurchased
-    template_name = 'products_purchased_add.html'
+    template_name = 'Products-Purchased/products_purchased_add.html'
     fields = ['customer', 'product', 'number_of_products_purchased', 'date_of_sale', 'amount_spent']
     success_url = reverse_lazy('dashboard')
 
 
 class ProductsPurchasedUpdateView(UpdateView):
     model = ProductsPurchased
-    template_name = 'products_purchased_edit.html'
+    template_name = 'Products-Purchased/products_purchased_edit.html'
     fields = ['customer', 'product', 'number_of_products_purchased', 'date_of_sale', 'amount_spent']
     success_url = reverse_lazy('dashboard')
 
 
 class ProductsPurchasedDeleteView(DeleteView):
     model = ProductsPurchased
-    template_name = 'products_purchased_confirm_delete.html'
+    template_name = 'Products-Purchased/products_purchased_confirm_delete.html'
     success_url = reverse_lazy('dashboard')
